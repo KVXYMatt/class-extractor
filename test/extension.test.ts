f607afea7278db9334a4a4519263d62b3fa6b836
Matthew Knight <mkni379@gmail.com>
2020-04-04 15:33:42 +1000

    Updated dependencies and migrated to new extension format

--- test/extension.test.ts
+++
@@ -1,8 +0,0 @@
-import * as assert from 'assert';
-
-import * as vscode from 'vscode';
-import * as htmlToCss from '../src/extension';
-
-suite("Extension Tests", () => {
-	// Test to come soon
-});
